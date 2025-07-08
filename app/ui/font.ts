388f820ab26e09c4ea956c39c389f4d5d1bad7ae
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-07-08 15:57:20 +0900

    拡張子の修正

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,6 +1,6 @@
 // /app/ui/fonts.ts
-import { Inter } from 'next/font/google';
+import { Inter , Lusitana} from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const  lusitana = Inter({subsets: ["latin"], weight: ["400","700"]})
+export const  lusitana = Lusitana({subsets: ["latin"], weight: ["400","700"]})
